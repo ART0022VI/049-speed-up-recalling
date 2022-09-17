07f5df518e70ba7adefea092e4fe8452c966e2b3
ART0022VI <artem.jidov@gmail.com>
2022-09-17 15:38:25 +0700

    Initial commit

---
+++ 049fix/plfix.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MEC;
+using Qurre;
+using Qurre.Events;
+using Qurre.API;
+using Qurre.API.Events;
+
+namespace _049fix
+{
+    public class plfix : Plugin
+    {
+        public override string Developer => "ГIеJIbмeнь#3519";
+        public override string Name => "049Fix";
+        public override Version Version => new Version(1, 0, 0);
+        public override void Disable()
+        {
+            Scp049.StartRecall -= Start;
+        }
+
+        public override void Enable()
+        {
+            Scp049.StartRecall += Start;
+        }
+        public void Start(StartRecallEvent ev)
+        {
+            ev.Target.Role = RoleType.Scp0492;
+            Timing.CallDelayed(0.5f, () => { ev.Target.Position = ev.Scp049.Position; });
+        }
+    }
+}
